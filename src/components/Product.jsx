bfcd03fc8ab088c39850f47379ccb6e08172f788
Angelica Garcia <anyegar36@gmail.com>
2023-04-21 17:39:55 -0500

    add styles

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,14 +8,14 @@ export function Product ({ img, productName, cost, quantity, userName, id }) {
   }
 
   return (
-    <div className='principal-container-products'>
+    <div className='max-w-[600px] w-[100%] h-[100px] grid grid-cols-4 place-content-center place-items-center rounded-2xl bg-[#ffffff] mt-[10px] shadow-[rgb(0 0 0 /9%)]'>
       <img
-        className='img-product'
+        className='h-[80px] w-[80px] rounded-full p-1'
         src={img}
       />
-      <p className='product'>{productName}</p>
-      <p className='cost'>${cost}.00</p>
-      <button className='add-products' onClick={handleAddToCheckout}>Añadir</button>
+      <p className='text-lg'>{productName}</p>
+      <p className='text-lg'>${cost}.00</p>
+      <button className='bg-[#FFD66C] rounded-2xl w-[80px] p-[.7rem] text-center border-0 mr-[10px] cursor-pointer' onClick={handleAddToCheckout}>Añadir</button>
     </div>
   )
 }
