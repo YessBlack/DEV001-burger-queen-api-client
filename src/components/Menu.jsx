08fc9d5d54556363bab7dd9c51d95767389ce498
Angelica Garcia <anyegar36@gmail.com>
2023-04-21 21:47:00 -0500

    change styles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -24,13 +24,13 @@ function Menu ({ useNavigate }) {
   return (
     <>
       <section className='bg-[#FFD66C]'>
-        <div className='grid grid-cols-2-menu gap-1 max-w-[1024px] m-auto bg-button-secondary-color'>
+        <div className='grid grid-cols-2 gap-1 max-w-[1024px] m-auto '>
           <button
-            className='w-[80%] bg-button-primary-color p-[20px] text-2xl font-bold cursor-pointer border-0 hover:scale-110 transition duration-200' onClick={handleClickBreakFast}
+            className='w-[100%] bg-button-primary-color p-[20px] text-2xl font-bold cursor-pointer border-0 hover:scale-110 transition duration-200' onClick={handleClickBreakFast}
           > Desayuno
           </button>
           <button
-            className='w-[70%] bg-button-secondary-color p-[20px] text-2xl font-bold cursor-pointer border-0 hover:scale-110 transition duration-200'
+            className='w-[100%] bg-button-secondary-color p-[20px] text-2xl font-bold cursor-pointer border-0 hover:scale-110 transition duration-200'
             onClick={handleClickLunchDinner}
           >
             Almuerzo/Cena
